57e0a7f7a50ff7458b25cf943d601225dd5ca831
kazumama <kazuma.ishida.kosuke@icloud.com>
2022-11-26 23:48:17 +0900

    スペル直し

--- database/seeders/CurriculumSeeder.php
+++ database/seeders/CurriculumSeeder.php
@@ -16,133 +16,133 @@ class CurriculamSeeder extends Seeder
      */
     public function run()
     {
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム01-1',
             'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム02-1',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム03-1',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム03-2',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム04-1',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム05-1',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム06-1',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム06-2',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム07-1',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム08-1',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-             DB::table('curriculams')->insert([
+             DB::table('curriculums')->insert([
             'name' => 'カリキュラム08-2',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-             DB::table('curriculams')->insert([
+             DB::table('curriculums')->insert([
             'name' => 'カリキュラム08-3',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-             DB::table('curriculams')->insert([
+             DB::table('curriculums')->insert([
             'name' => 'カリキュラム08-4',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-             DB::table('curriculams')->insert([
+             DB::table('curriculums')->insert([
             'name' => 'カリキュラム08-5',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-             DB::table('curriculams')->insert([
+             DB::table('curriculums')->insert([
             'name' => 'カリキュラム08-6',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-             DB::table('curriculams')->insert([
+             DB::table('curriculums')->insert([
             'name' => 'カリキュラム09-1',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム09-2',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム09-3',
              'vote_count' => 0,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
             ]);
             
-            DB::table('curriculams')->insert([
+            DB::table('curriculums')->insert([
             'name' => 'カリキュラム09-4',
              'vote_count' => 0,
             'created_at' => new DateTime(),
