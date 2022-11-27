174d9d3e7808d7539aefea63028f18690d0b9424
kazumama <kazuma.ishida.kosuke@icloud.com>
2022-11-27 17:18:42 +0900

    CSS

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,13 +7,16 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
+        <div style= 'text-align:center; '>
         <h1>エラーを解決しましょう！</h1>
         <h2>カリキュラム一覧画面</h2>
+        
         <a href='/posts/create'>新規投稿</a>
+        </div>
         <div>
             @foreach ($curriculums as $curriculum)
-                <div style='border:solid 1px; margin-bottom: 10px;'>
-                    <p>
+                <div style='border:solid 1px; width: 400px; margin: 0 auto; margin-bottom: 10px;  text-align:center;  padding:20px 0; background-color: #00FF00; '>
+                        <p>
                     　　<a href="/posts/{{ $curriculum->id }}">{{ $curriculum->name }}</a>
                     </p>
                 </div>
