d92d1141bea34a31186f1351839e678b23370d6f
kazumama <kazuma.ishida.kosuke@icloud.com>
2022-11-27 12:22:02 +0900

    create,show,edit

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -7,16 +7,16 @@
     <body>
         <h1 class="title">編集画面</h1>
         <div class="content">
-            <form action="/posts/{{ $post->id }}" method="POST">
+            <form action="/posts/{{ $review->id }}" method="POST">
                 @csrf
                 @method('PUT')
                 <div class='content__title'>
                     <h2>タイトル</h2>
-                    <input type='text' name='post[title]' value="{{ $post->title }}">
+                    <input type='text' name='review[title]' value="{{ $review->title }}">
                 </div>
                 <div class='content__body'>
                     <h2>本文</h2>
-                    <input type='text' name='post[body]' value="{{ $post->body }}">
+                    <input type='text' name='review[body]' value="{{ $review->body }}">
                 </div>
                 <input type="submit" value="保存">
             </form>
